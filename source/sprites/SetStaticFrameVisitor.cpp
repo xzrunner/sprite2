e023bfffe48e215e22d7a5e8dfe838136fd50b65
zz <zhuguang@ejoy.com>
2017-05-03 19:34:41 +0800

    [FIXED] set frame

--- source/sprites/SetStaticFrameVisitor.cpp
+++ source/sprites/SetStaticFrameVisitor.cpp
@@ -16,10 +16,16 @@ SetStaticFrameVisitor::SetStaticFrameVisitor(int static_frame)
 }
 
 VisitResult SetStaticFrameVisitor::Visit(const Sprite* spr, const SprVisitorParams& params)
-{
-	VisitResult ret = VISIT_OVER;
-
+{	
 	SymType type = static_cast<SymType>(spr->GetSymbol()->Type());
+	if (type == SYM_ANCHOR) {
+		return VISIT_INTO;
+	}
+	if (!spr->IsInheritUpdate()) {
+		return VISIT_OVER;
+	}
+
+	VisitResult ret = VISIT_OVER;
 	switch (type)
 	{
 	case SYM_ANIMATION:
@@ -35,11 +41,10 @@ VisitResult SetStaticFrameVisitor::Visit(const Sprite* spr, const SprVisitorPara
 			anim2_spr->SetStaticTime(s2::UpdateParams(), m_static_frame);
 		}
 		break;
-	case SYM_ANCHOR: case SYM_COMPLEX: case SYM_MESH: case SYM_MASK: case SYM_PROXY:
+	case SYM_COMPLEX: case SYM_MESH: case SYM_MASK: case SYM_PROXY:
 		ret = VISIT_INTO;
 		break;
 	}
-
 	return ret;
 }
 

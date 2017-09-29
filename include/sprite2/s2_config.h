74c96061f8e6743bc730f9006b615a8ca4b619ef
zz <zhuguang@ejoy.com>
2017-09-29 11:34:06 +0800

    fix config

--- include/sprite2/s2_config.h
+++ include/sprite2/s2_config.h
@@ -1,4 +1,14 @@
-#define S2_VIRTUAL_INHERITANCE
+//#define S2_VIRTUAL_INHERITANCE
+
+#define S2_SPR_DEFAULT_EDITABLE false
+
+//#define S2_MATRIX_FIX
+
+//#define S2_SPR_CACHE_LOCAL_MAT_SHARE
+
+//#define S2_RES_LOG
+
+#define S2_DISABLE_MODEL
 
 #ifdef NDEBUG
 #define S2_DISABLE_STATISTICS

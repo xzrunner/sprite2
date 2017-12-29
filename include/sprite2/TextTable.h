4b0ec4af2cb30c0a42df36c3810e5d35677d0caa
zz <zhuguang@ejoy.com>
2017-12-29 09:54:39 +0800

    [ADDED] text table db, and fix lang index
    Conflicts:
    	include/sprite2/TextTable.h
    	include/sprite2/s2_config.h
    	source/sprites/TextTable.cpp

--- include/sprite2/TextTable.h
+++ include/sprite2/TextTable.h
@@ -1,34 +1,47 @@
 #ifndef _SPRITE2_TEXT_TABLE_H_
 #define _SPRITE2_TEXT_TABLE_H_
 
+#include "sprite2/config.h"
+
 #include <cu/cu_stl.h>
 #include <cu/cu_macro.h>
+#ifdef S2_TEXT_TABLE_DB
+#include <playdb/btree/BTree.h>
+#endif // S2_TEXT_TABLE_DB
 
 namespace s2
 {
 
 class TextTable
 {
-public:
-	enum LanguageIdx
-	{
-		LANG_SIMPLIFIED = 0,
-		LANG_TRADITIONAL = 1,
+public:	
+	void SetTypes(const CU_VEC<CU_STR>& types) {
+		m_types = types;
+	}
 
-		LANG_COUNT
-	};
+	CU_STR Query(int lang, const CU_STR& tid) const;
 
-public:
+	int QueryType(const CU_STR& lang) const;
+
+#ifdef S2_TEXT_TABLE_DB
+	void LoadDB(const CU_STR& index_path, const CU_STR& data_path);
+#else
 	void Insert(const CU_STR& tid, const CU_VEC<CU_STR>& languages);
-	
-	CU_STR Query(LanguageIdx lang, const CU_STR& tid) const;
 
 	void Clear();
 
 	bool IsEmpty() const { return m_texts.empty(); }
+#endif // S2_TEXT_TABLE_DB
 
 private:
+	CU_VEC<CU_STR> m_types;
+
+#ifdef S2_TEXT_TABLE_DB
+	std::unique_ptr<playdb::IStorageManager> m_storage_mgr;
+	std::unique_ptr<playdb::btree::BTree<CU_STR>> m_tree;
+#else
 	CU_MAP<CU_STR, CU_VEC<CU_STR>> m_texts;
+#endif // S2_TEXT_TABLE_DB
 
 	CU_SINGLETON_DECLARATION(TextTable)
 
--- source/sprites/TextTable.cpp
+++ source/sprites/TextTable.cpp
@@ -1,5 +1,10 @@
 #include "sprite2/TextTable.h"
 
+#ifdef S2_TEXT_TABLE_DB
+#include <bimp/BIMP_ImportStream.h>
+#include <playdb/storage/DiskStorageManager.h>
+#endif // S2_TEXT_TABLE_DB
+
 #include <fstream>
 
 namespace s2
@@ -11,14 +16,47 @@ TextTable::TextTable()
 {
 }
 
-void TextTable::Insert(const CU_STR& tid, const CU_VEC<CU_STR>& languages)
+int TextTable::QueryType(const CU_STR& lang) const
 {
-	m_texts.insert(std::make_pair(tid, languages));
+	for (size_t i = 0, n = m_types.size(); i < n; ++i) {
+		if (m_types[i] == lang) {
+			return i;
+		}
+	}
+	return -1;
 }
 
-CU_STR TextTable::Query(LanguageIdx lang, const CU_STR& tid) const
+#ifdef S2_TEXT_TABLE_DB
+
+CU_STR TextTable::Query(int lang, const CU_STR& tid) const
 {
-	if (lang < 0 || lang >= LANG_COUNT) {
+	playdb::btree::Data<CU_STR> data;
+	if (!m_tree->Query(tid, data)) {
+		return "";
+	}
+
+	bimp::ImportStream is((const char*)(data.data), data.data_len);
+	uint8_t num = is.UInt8();
+	std::vector<CU_STR> langs;
+	langs.reserve(num);
+	for (int i = 0; i < num; ++i) {
+		langs.push_back(is.LongString());
+	}
+	return langs[lang];
+}
+
+void TextTable::LoadDB(const CU_STR& index_path, const CU_STR& data_path)
+{
+	m_storage_mgr = std::make_unique<playdb::storage::DiskStorageManager>(
+		index_path, data_path);
+	m_tree = std::make_unique<playdb::btree::BTree<CU_STR>>(m_storage_mgr.get());
+}
+
+#else
+
+CU_STR TextTable::Query(int lang, const CU_STR& tid) const
+{
+	if (lang < 0) {
 		return "";
 	}
 
@@ -33,9 +71,16 @@ CU_STR TextTable::Query(LanguageIdx lang, const CU_STR& tid) const
 	return itr->second[lang];
 }
 
+void TextTable::Insert(const CU_STR& tid, const std::vector<CU_STR>& languages)
+{
+	m_texts.insert(std::make_pair(tid, languages));
+}
+
 void TextTable::Clear()
 {
 	m_texts.clear();
 }
+
+#endif // S2_TEXT_TABLE_DB
 
 }

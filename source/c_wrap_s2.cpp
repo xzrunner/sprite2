c55061506b2b61c5578acf2b9acbaf3272478c3f
zz <zhuguang@ejoy.com>
2017-12-31 08:11:28 +0800

    rename

--- source/c_wrap_s2.cpp
+++ source/c_wrap_s2.cpp
@@ -54,7 +54,7 @@
 #include <shaderlab/Sprite2Shader.h>
 #include <shaderlab/FilterShader.h>
 #include <shaderlab/Statistics.h>
-#include <bsn/ColorParser.h>
+#include <sns/ColorParser.h>
 
 #include <cooking/DisplayList.h>
 
@@ -1622,7 +1622,7 @@ void s2_rvg_draw_circle(bool filling, float cx, float cy, float radius, int segm
 extern "C"
 uint32_t s2_trans_color(uint32_t src, enum S2_PIXEL_TYPE src_type, enum S2_PIXEL_TYPE dst_type)
 {
-	return bsn::ColorParser::Trans(src, bsn::PIXEL_TYPE(src_type), bsn::PIXEL_TYPE(dst_type));
+	return sns::ColorParser::Trans(src, sns::PIXEL_TYPE(src_type), sns::PIXEL_TYPE(dst_type));
 }
 
 extern "C"

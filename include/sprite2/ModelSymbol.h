266acd36a05c44bde46ddf995d1e41d8cac77249
zz <zhuguang@ejoy.com>
2018-01-28 05:36:31 +0800

    [CHANGED] move some code from node3 to node0

--- include/sprite2/ModelSymbol.h
+++ include/sprite2/ModelSymbol.h
@@ -8,7 +8,8 @@
 #include "sprite2/Symbol.h"
 #include S2_MAT_HEADER
 
-#include <node3/SceneNode.h>
+#include <node0/SceneNode.h>
+#include <SM_Matrix.h>
 
 namespace s2
 {
@@ -29,8 +30,8 @@ public:
 	virtual RenderReturn DrawNode(cooking::DisplayList* dlist, const RenderParams& rp, const Sprite* spr, ft::FTList& ft, int pos) const override { return RENDER_SKIP; }
 #endif // S2_DISABLE_FLATTEN
 
-	void SetNode(const n3::SceneNodePtr& node) { m_node = node; }
-	const n3::SceneNodePtr& GetNode() const { return m_node; }
+	void SetNode(const n0::SceneNodePtr& node) { m_node = node; }
+	const n0::SceneNodePtr& GetNode() const { return m_node; }
 
 protected:
 	virtual sm::rect GetBoundingImpl(const Sprite* spr = nullptr, const Actor* actor = nullptr, bool cache = true) const override;
@@ -40,7 +41,7 @@ private:
 	static sm::mat4 CalcCam3dMat();
 
 protected:
-	n3::SceneNodePtr m_node = nullptr;
+	n0::SceneNodePtr m_node = nullptr;
 
 }; // ModelSymbol
 
